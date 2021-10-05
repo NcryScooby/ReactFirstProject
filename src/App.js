076f995de5bc968f031535054bea6b586ef9e362
NcryScooby <fabricio_c_anelli@hotmail.com>
2021-10-05 19:17:34 -0300

    #2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
+import React from "react";
+import { ListaDeNotas } from "./componentes/listaDeNotas";
+
 function App() {
   return (
     <>
-    <form>
-      <input type="text" placeholder="Título"/>
-        <textarea placeholder="Escreva sua nota..."/>
+      <form>
+        <input type="text" placeholder="Título" />
+        <textarea placeholder="Escreva sua nota..." />
         <button>Criar Nota</button>
-    </form>
-
-      <ul>
-        <li>
-          <section>
-            <header>
-              <h3>Título</h3>
-            </header>
-            <p>Escreva sua nota</p>
-          </section>
-        </li>
-      </ul>
-      </>
+      </form>
+      <ListaDeNotas/>
+    </>
   );
 }
 
---
+++ src/componentes/listaDeNotas.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Component } from "react/cjs/react.production.min";
+
+export class ListaDeNotas extends Component {
+  render() {
+    return (
+      <>
+        <ul>
+          <li>
+            <section>
+              <header>
+                <h3>Título</h3>
+              </header>
+              <p>Escreva sua nota</p>
+            </section>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <section>
+              <header>
+                <h3>Título</h3>
+              </header>
+              <p>Escreva sua nota</p>
+            </section>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <section>
+              <header>
+                <h3>Título</h3>
+              </header>
+              <p>Escreva sua nota</p>
+            </section>
+          </li>
+        </ul>
+      </>
+    );
+  }
+}
